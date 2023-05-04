67dd9b042e710e068808a7af4dce92e6ba21e371
Coder1804 <samsunga40uzb@gmail.com>
2023-05-04 15:33:09 +0500

    env file dagi buglar to'girlandi

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ let apiCache = require('apicache')
 // Env vars 
 
 const API_BASE_URL = process.env.API_BASE_URL;
-const API_KEY_NAME = process.env.API_BASE_NAME;
-const API_BASE_VALUE = process.env.API_BASE_VALUE;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
 apiCache = apiCache.middleware
 
 router.get('/' , apiCache('2 minutes'),async (req, res)=>{
    try {
     const params = new URLSearchParams({
-        [API_KEY_NAME] : API_BASE_VALUE,
+        [API_KEY_NAME] : API_KEY_VALUE,
         ...url.parse(req.url,true).query
     })
 
